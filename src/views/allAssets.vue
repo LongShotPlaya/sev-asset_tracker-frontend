ec0b0d8fa632416cde60feccf8acc8172e45a5a1
LongShotPlaya <J.Nixon@eagles.oc.edu>
2024-02-05 20:14:09 -0600

    Updating before my computer dies lol

--- src/views/allAssets.vue
+++ src/views/allAssets.vue
@@ -2,46 +2,76 @@
 
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
+import Utils from "../config/utils.js";
 
 const router = useRouter();
 const user = Utils.getStore("user");
-const message = ref("");
+const message = ref("Everything is working");
 
-const id = ref();
-const search = ref("");
-const assets = ref([]);
-const name = ref("");
-const type = ref("");
-const catergory = ref("");
+const asset = ref({
+    id: ref(),
+    returnDate: ref(),
+    borrowerId: ref(),
+    type: ref(""),
+    catergory: ref(""),
+})
 
 
-onMounted();
+const getAssets = () => {
+    const data = {
+        id: asset.value.id,
+        returnDate: asset.value.returnDate,
+        borrowerId: asset.value.borrowerId,
+    };
+};
+
+
+onMounted(() => {
+    user.value = Utils.getStore("user");
+});
 
 </script>
 
 <template>
     <v-container>
 		<v-toolbar>
-			<v-toolbar-title>{{ user + " All Assets" }}</v-toolbar-title>
+			<v-toolbar-title>{{ "All Assets" }}</v-toolbar-title>
 		</v-toolbar>
 
         <!--List of all assets-->
-            <div class="container">
-                <v-form ref="form" v-model="valid" lazy validation>
-                    <v-text-field required v-model="asset.id" id="id" label="ID">
+            <v-card>
+                <v-card-text>
+                    <b>{{ message }}</b>
+                    <h4>{{ user.fName }}</h4>
+                </v-card-text>
 
-                    </v-text-field>
-                    <v-text-field required v-model="asset.name" id="name" label="Name">
+                <v-row justify="flex-end">  
+                    <v-btn class="mx-2" color="secondary" @click="saveChanges">Add Asset</v-btn>
+                </v-row>
 
-                    </v-text-field>
-                    <v-text-field required v-model="asset.type" id="type" label="Type">
+                <br>
 
-                    </v-text-field>
-                    <v-text-field required v-model="asset.catergory" id="catergory" label="Catergory">
+                <v-table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Return Date</th>
+                            <th>Current Owner</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(asset, index) in assets" :key="index">
+                            <td>{{ `${asset.id}` }}</td>
+                            <td>{{ `${asset.returnDate}`}}</td>
+                            <td>{{ `${asset.borrowerId}`}}</td>
+                        </tr>
+                    </tbody>
+                </v-table>
 
-                    </v-text-field>
-                </v-form>
-            </div>
+                <v-table>
+
+                </v-table>
+            </v-card>
     </v-container>
 </template>
 
