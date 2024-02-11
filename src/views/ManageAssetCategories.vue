8e9e63038166439081460cd918373e3355e6efd0
Jessica-Vaughan <jessica.vaughan@eagles.oc.edu>
2024-02-11 17:57:30 -0600

    Set up a rough draft of the MAC page.

--- src/views/ManageAssetCategories.vue
+++ src/views/ManageAssetCategories.vue
@@ -1,93 +1,119 @@
-
 <template>
-    <v-row justify="center">
-      <v-dialog
-        v-model="dialog"
-        fullscreen
-        :scrim="false"
-        transition="dialog-bottom-transition"
-      >
-        <template v-slot:activator="{ props }">
-          <v-btn
-            color="primary"
-            dark
-            v-bind="props"
-          >
-            Open Dialog
+  <v-toolbar>
+    <v-toolbar-title>Manage Asset Categories</v-toolbar-title>
+  </v-toolbar>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">
+          Name
+        </th>
+        <th class="text-left">
+          Description
+        </th>
+        <th class="text-left">
+          Actions
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in categories" :key="item.name">
+        <td>{{ item.name }}</td>
+        <td>{{ item.description }}</td>
+        <td>
+          <v-btn color="primary" @click="openDialog(item)">
+            Edit
           </v-btn>
-        </template>
-        <v-card>
-          <v-toolbar
-            dark
-            color="primary"
-          >
-            <v-btn
-              icon
-              dark
-              @click="dialog = false"
-            >
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-            <v-toolbar-title>Settings</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-toolbar-items>
-              <v-btn
-                variant="text"
-                @click="dialog = false"
-              >
-                Save
-              </v-btn>
-            </v-toolbar-items>
-          </v-toolbar>
-          <v-list
-            lines="two"
-            subheader
-          >
-            <v-list-subheader>User Controls</v-list-subheader>
-            <v-list-item title="Content filtering" subtitle="Set the content filtering level to restrict apps that can be downloaded"></v-list-item>
-            <v-list-item title="Password" subtitle="Require password for purchase or use password to restrict purchase"></v-list-item>
-          </v-list>
-          <v-divider></v-divider>
-          <v-list
-            lines="two"
-            subheader
-          >
-            <v-list-subheader>General</v-list-subheader>
-            <v-list-item title="Notifications" subtitle="Notify me about updates to apps or games that I downloaded">
-              <template v-slot:prepend>
-                <v-checkbox v-model="notifications"></v-checkbox>
-              </template>
-            </v-list-item>
-            <v-list-item title="Sound" subtitle="Auto-update apps at any time. Data charges may apply">
-              <template v-slot:prepend>
-                <v-checkbox v-model="sound"></v-checkbox>
-              </template>
-            </v-list-item>
-            <v-list-item title="Auto-add widgets" subtitle="Automatically add home screen widgets">
-              <template v-slot:prepend>
-                <v-checkbox v-model="widgets"></v-checkbox>
-              </template>
-            </v-list-item>
-          </v-list>
-        </v-card>
-      </v-dialog>
-    </v-row>
-  </template>
-  <script>
-    export default {
-      data () {
-        return {
-          dialog: false,
-          notifications: false,
-          sound: true,
-          widgets: false,
-        }
-      },
-    }
-  </script>
-  <style>
-  .dialog-bottom-transition-enter-active,
-  .dialog-bottom-transition-leave-active {
-    transition: transform .2s ease-in-out;
-  }
-  </style>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
+
+  <v-dialog v-model="dialog" persistent max-width="800px">
+    <v-card>
+      <v-card-title class="text-h5">
+        <v-text-field
+          v-model="item.name"
+          id="name"
+          :counter="50"
+          label="Name"
+          required
+        ></v-text-field>
+      </v-card-title>
+      <v-text-field
+        v-model="item.description"
+        id="description"
+        :counter="50"
+        label="Description"
+        required
+      ></v-text-field>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="green-darken-1" variant="text" @click="saveChanges">
+          Save
+        </v-btn>
+        <v-btn color="green-darken-1" variant="text" @click="closeDialog">
+          Cancel
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <!-- Add button to trigger the "Add" functionality -->
+  <v-btn color="primary" @click="addCategory">
+    Add
+  </v-btn>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      categories: [
+        {
+          name: 'Vehicle',
+          description: 'For vehicle-based assets. This description is super long because I want to see if the text wraps. If this keeps going than it did not wrap which will need to be fixed because no one wants a really wide web page.',
+        },
+        {
+          name: 'Electronics',
+          description: 'For electronic-based assets.',
+        },
+        {
+          name: 'Building',
+          description: 'For building-based assets.',
+        },
+      ],
+      dialog: false,
+      item: {},
+    };
+  },
+  methods: {
+    openDialog(item) {
+      this.item = { ...item };
+      this.dialog = true;
+    },
+    saveChanges() {
+      // Handle save logic here
+      if (this.categories.includes(this.item)) {
+        // Update existing item
+        const index = this.categories.indexOf(this.item);
+        this.$set(this.categories, index, { ...this.item });
+      } else {
+        // Add new item
+        this.categories.push({ ...this.item });
+      }
+
+      this.closeDialog();
+    },
+    closeDialog() {
+      this.dialog = false;
+      this.item = {}; // Reset item
+    },
+    addCategory() {
+      // Initialize item for adding a new category
+      this.item = { name: '', description: '' };
+      this.dialog = true;
+    },
+  },
+};
+</script>
