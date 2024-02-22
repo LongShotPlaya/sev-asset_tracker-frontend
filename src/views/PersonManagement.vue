541313a8d80661d85de367b066188ea65725c420
LongShotPlaya <J.Nixon@eagles.oc.edu>
2024-02-22 15:02:36 -0600

    Got people list working

--- src/views/PersonManagement.vue
+++ src/views/PersonManagement.vue
@@ -1,28 +1,22 @@
 <script setup>
 
 import { ref, onMounted } from "vue";
-// import { useRouter } from "vue-router";
+import { useRouter } from "vue-router";
 import Utils from "../config/utils.js";
 import peopleServices from "../services/personServices";
 
-// const router = useRouter();
+const router = useRouter();
 const user = Utils.getStore("user");
-const message = ref("Everything is working");
+const message = ref("");
 const search = ref("");
-const headers = ref("");
-let people = [];
 const person = ref("");
+let people = ref([]);
 
-const desserts = ref("");
-
-const getAllPeople = () => {
-  people = [];
+const getPeople = () => { 
   peopleServices.getAllPeople()
     .then((response) => {
       person.value = response.data;
-      person.value.forEach((currentPerson) => {
-        people.push(currentPerson.role);
-      });
+      people.value = response.data;
     })
     .catch((e) => {
       message.value = e.response.data.message;
@@ -31,9 +25,8 @@ const getAllPeople = () => {
 
 onMounted(() => {
   user.value = Utils.getStore("user");
-  getAllPeople();
+  getPeople();
 });
-
 
 </script>
 
@@ -50,22 +43,46 @@ onMounted(() => {
       variant="outlined"
       hide-details
     ></v-text-field>
-
-  <v-data-table
-    :headers="headers"
-    :items="desserts"
-    :search="search"
-  ></v-data-table>
 </v-card>
 
-            
+
+<v-card class="table">
+    <v-table>
+      <thead>
+        <tr>
+          <th class="text-left column">
+            Name
+          </th>
+          <th class="text- column">
+            Email
+          </th>
+          <th class="text-left column">
+            Other Things...
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="person in people" :key="person.id">
+          <td class="column">{{ person.fName + ' ' + person.lName}}</td>
+          <td class="column">{{ person.email }}</td>
+          <td class="column">
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-card> 
     
 </template>
 
 <style scoped>
     .title{
-        padding-left: 5%;
-        padding-right: 5%;
+      padding-left: 5%;
+      padding-right: 5%;
+    }
+    .table{
+      margin-left: 5%;
+      margin-right: 5%;
+      margin-top: 1%;
     }
 
 </style>
