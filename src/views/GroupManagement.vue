1bcf183754bb5c6b57592da427e80fcd4a3b271c
WilsonTrin <wilsontrin45@gmail.com>
2024-02-26 18:46:49 -0600

    Commit to switch
    Commiting this before I switch it has basic functionality and better style

--- src/views/GroupManagement.vue
+++ src/views/GroupManagement.vue
@@ -2,6 +2,7 @@
 	import Utils from "../config/utils.js";
 	import { ref, onMounted } from "vue";
 	import AuthServices from "../services/authServices";
+	import groupServices from "../services/groupServices";
 	import { useRouter } from "vue-router";
 
 	const router = useRouter();
@@ -26,31 +27,37 @@
 
 </script>
 
-<template>
+<template v-slot:activator="{ props: activatorProps }">
     <v-container>
-		<v-row class="mt-7 mx-0" justify="left">
-            <v-btn color="primary" @click="router.push({name: ''})">Add group</v-btn>
-        </v-row>
+        <v-btn class="mt-7 mx-0" color="primary" v-bind="activatorProps">Add group</v-btn>
 		<v-card class="mt-7" variant="outlined" color="grey-lighten-1">
 			<v-table>
 				<thead>
 				<tr>
 					<th class="text-center">Name</th>
-					<th class="text-center">Description</th>
+					<th class="text-center">Number of Users</th>
 					<th class="text-center">Actions</th>
 				</tr>
 				</thead>
 				<tbody>
 				<tr v-for="(item, index) in groups" :key="index">
-					<td>{{ item.name }}</td>
-					<td>{{ item.description }}</td>
-					<td>
-					<v-icon small class="mx-4" @click="router.push({name: ''})">
-					mdi-format-list-bulleted-type</v-icon>
-					<v-icon small class="mx-4" @click="router.push({name: ''})">
-					mdi-pencil</v-icon>
-					<v-icon small class="mx-4" @click="router.push({name: ''})">
-					mdi-trash-can</v-icon>
+					<td class="text-center">{{ item.name }}</td>
+					<td class="text-center">Unimplemented</td>
+					<td class="text-center">
+						<v-row justify="center">
+						<div>
+							<v-btn size="small" icon="mdi-format-list-bulleted-type" @click="router.push({name: ''})">
+							</v-btn><v-tooltip activator="parent" location="bottom">View Group</v-tooltip>
+						</div>
+						<div>
+							<v-btn size="small" icon="mdi-pencil" @click="router.push({name: ''})">
+							</v-btn><v-tooltip activator="parent" location="bottom">Edit Group</v-tooltip>
+						</div>
+						<div>
+							<v-btn size="small" icon="mdi-trash-can" @click="router.push({name: ''})">
+							</v-btn><v-tooltip activator="parent" location="bottom">Delete Group</v-tooltip>
+						</div>
+					</v-row>
 					</td>
 				</tr>
 				</tbody>
@@ -60,4 +67,33 @@
 			<b>{{ message }}</b>
 		</v-card-text>
     </v-container>
+
+	<v-dialog max-width="500">
+  		<template v-slot:activator="{ props: activatorProps }">
+			<v-btn
+				v-bind="activatorProps"
+				color="surface-variant"
+				text="Open Dialog"
+				variant="flat">
+			</v-btn>
+  		</template>
+
+		<template v-slot:default="{ isActive }">
+			<v-card title="Dialog">
+			<v-card-text>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			</v-card-text>
+
+			<v-card-actions>
+				<v-spacer></v-spacer>
+
+				<v-btn
+				text="Close Dialog"
+				@click="isActive.value = false"
+				></v-btn>
+			</v-card-actions>
+			</v-card>
+		</template>
+	</v-dialog>
+
 </template>
