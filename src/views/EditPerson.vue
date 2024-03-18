d08117fbdfdb263e6bb215f8a7c23969dc9d593b
LongShotPlaya <J.Nixon@eagles.oc.edu>
2024-03-17 19:53:34 -0500

    layout made and som functionality

--- src/views/EditPerson.vue
+++ src/views/EditPerson.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { ref, onMounted, defineProps } from "vue";
 import { useRouter } from "vue-router";
-import Utils from "../config/utils.js";
 import peopleServices from "../services/personServices";
+import assetServices from "../services/assetServices.js";
 
 const message = ref("");
-const person = ref(null);
+const person = ref("");
+const personsAssets = ref([]);
 
 const props = defineProps({
   id: {
@@ -23,30 +24,101 @@ const getSomeone = (id) => {
         })
 };
 
+const getPersonsAssets = (id) => {
+    assetServices.getAllAssets()
+    .then((response) => {
+        personsAssets.value = response.data.filter(asset => asset.borrowerId === id);
+        console.log(personsAssets);
+    })
+    .catch((error) => {
+        message.value = error.response.data.message;
+    })
+};
+
 onMounted(() => {
     const id = props.id; 
     getSomeone(id);
-})
+    getPersonsAssets(id);
+});
 
-
+//Asset data table
+const headers = ref([
+    { title: 'Asset Name', value: 'id' },
+]);
 </script>
 
 <template>
+    <br><br>
     <v-card
     class="mx-auto"
     max-width="90%"
-    max-height="60%"
+    max-height="75%"
     >
         <v-app>
             <v-toolbar>
-                <v-toolbar-title>Found Person</v-toolbar-title>
+                <v-toolbar-title>{{ person.fName + ' ' + person.lName }}</v-toolbar-title>
             </v-toolbar>
 
+            <v-container>
+                <v-row>
+                    <v-col cols="3">
+                        <v-card class="first">
+                            <v-card-title>Person Role: {{ person.id }}</v-card-title>
+                            <v-divider></v-divider>
+                            <table id="contact">
+                                <tr>
+                                    <td>ID Number: </td>
+                                    <td>{{ person.id }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Email: </td>
+                                    <td>{{ person.email }}</td>
+                                </tr>
+                            </table>
+                        </v-card>
+                        <br>
 
+                        <v-card class="last">
+                        <v-card-text>Label for third block</v-card-text>
+                        <v-divider></v-divider>
+                        <v-card-title>Block 3</v-card-title>
+                        </v-card>
+                    </v-col>
+
+                    <v-col cols="9">
+                        <v-card class="list">
+                        <v-card-title>Assets</v-card-title>
+                        <v-divider></v-divider>
+                        <v-data-table
+                        :headers="headers"
+                        :items="items"
+                        :items-per-page="5"
+                        :hide-default-footer="true"
+                        >
+                        </v-data-table>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-app>
     </v-card>
 </template>
 
 <style scoped>
+
+.first{
+    min-height: 288px;
+}
+.list{
+    min-height: 600px;
+}
+.last{
+    min-height: 288px;
+    justify-content: center;
+    text-align: center;
+}
+#contact{
+    margin: 5%;
+}
 
 </style>
