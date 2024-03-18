4e7086b1ad524952e19347e8d642f4cf64ee8db6
LongShotPlaya <J.Nixon@eagles.oc.edu>
2024-03-17 22:28:02 -0500

    potential update

--- src/views/EditPerson.vue
+++ src/views/EditPerson.vue
@@ -1,55 +1,3 @@
-<script setup>
-import { ref, onMounted } from "vue";
-import { useRouter } from "vue-router";
-import peopleServices from "../services/personServices";
-import assetServices from "../services/assetServices.js";
-
-const message = ref("");
-const person = ref("");
-const personsAssets = ref([]);
-
-const props = defineProps({
-  id: {
-    required: true,
-  },
-});
-
-const getSomeone = (id) => {
-    peopleServices.getPerson(id) 
-        .then((response) => {
-            person.value = response.data;
-        })
-        .catch((error) => {
-            message.value = error.response.data.message;
-        })
-};
-
-const getPersonsAssets = (id) => {
-    assetServices.getAllAssets()
-    .then((response) => {
-        personsAssets.value = response.data.filter(asset => asset.borrowerId === id);
-        console.log(personsAssets);
-    })
-    .catch((error) => {
-        message.value = error.response.data.message;
-    })
-};
-
-//Asset data table
-const headers = ref([
-    { title: 'ID', value: 'id' },
-    { title: 'Name', value: 'name' },
-    { title: 'Type', value: 'type' },
-    { title: '', value: 'actions', align: 'end' },
-]);
-
-onMounted(() => {
-    const id = props.id; 
-    getSomeone(id);
-    getPersonsAssets(id);
-});
-</script>
-
 <template>
     <br><br>
     <v-card
@@ -99,24 +47,93 @@ onMounted(() => {
                         :items="personsAssets"
                         :page.sync="page"
                         :items-per-page="itemsPerPage"
-                        hide-default-footer
+                        no-footer
                         class="elevation-1"
                         @page-count="pageCount = $event"
-                        ></v-data-table>
+                        >
+                            <template #pagination="{ options, prev, next, page, pages }">
+                                <v-pagination
+                                    :length="pages"
+                                    :total-visible="10"
+                                    :prev-icon="null"
+                                    :next-icon="null"
+                                    @input="onPageChange"
+                                ></v-pagination>
+                            </template>
+                        </v-data-table>
                         <div class="text-center pt-2">
                             <v-pagination
-                            v-model="page"
-                            :length="pageCount"
-                            >
-                        </v-pagination>
+                                v-model="page"
+                                :length="pageCount"
+                            ></v-pagination>
                         </div>
-                        </v-card>
+                    </v-card>
                     </v-col>
                 </v-row>
             </v-container>
         </v-app>
     </v-card>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import peopleServices from "../services/personServices";
+import assetServices from "../services/assetServices.js";
+
+const message = ref("");
+const person = ref("");
+const personsAssets = ref([]);
+const page = ref(1); // Initialize page number
+const itemsPerPage = ref(5); // Define items per page
+const pageCount = ref(0); // Initialize page count
+
+const props = defineProps({
+  id: {
+    required: true,
+  },
+});
+
+const getSomeone = (id) => {
+    peopleServices.getPerson(id) 
+        .then((response) => {
+            person.value = response.data;
+        })
+        .catch((error) => {
+            message.value = error.response.data.message;
+        })
+};
+
+const getPersonsAssets = (id) => {
+    assetServices.getAllAssets()
+    .then((response) => {
+        personsAssets.value = response.data.filter(asset => asset.borrowerId === id);
+        console.log(personsAssets);
+    })
+    .catch((error) => {
+        message.value = error.response.data.message;
+    })
+};
+
+//Asset data table
+const headers = ref([
+    { title: 'ID', value: 'id' },
+    { title: 'Name', value: 'name' },
+    { title: 'Type', value: 'type' },
+    { title: '', value: 'actions', align: 'end' },
+]);
+
+const onPageChange = (page) => {
+    console.log('Current Page:', page);
+    // You can perform any additional logic here when the page changes
+};
+
+onMounted(() => {
+    const id = props.id; 
+    getSomeone(id);
+    getPersonsAssets(id);
+});
+</script>
 
 <style scoped>
 
