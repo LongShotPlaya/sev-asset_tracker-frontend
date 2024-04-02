720facb54c6ab7807e4b712b807e0f725c483e45
Vyryty <colonelsuperawesome@gmail.com>
2024-04-02 05:29:34 -0500

    ref: Small visual bug fix

--- src/views/AssetCatManagement.vue
+++ src/views/AssetCatManagement.vue
@@ -22,7 +22,7 @@
             item-key ="id"
           >
             <template v-slot:[`item.actions`]="{ item }">
-              <v-btn class="ma-2" color="primary" icon="mdi-pencil" size="small" @click="openDialog(item)">
+              <v-btn class="ma-2" color="primary" icon="mdi-pencil" @click="openDialog(item)">
                 <v-icon>mdi-pencil</v-icon>
               </v-btn>
               <v-btn
