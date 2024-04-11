2ff5ace6622ed40a7f496025fd9adcf452bca08f
Vyryty <colonelsuperawesome@gmail.com>
2024-04-10 23:19:05 -0500

    feat: Some search functionality
    - User can now search for assets by their specific fields
    - User can now see if items are checked out to a person and go to their profile if so
    - Assets are sorted better

--- src/views/AllAssets.vue
+++ src/views/AllAssets.vue
@@ -44,7 +44,129 @@ const search = ref(null);
 
 const assets = ref([]);
 const filteredAssets = computed(() => {
-    return assets.value;
+    if (search.value == "") search.value = null;
+    // category=category name, type=type name, identifier=, location=building or person name
+    
+    const filters = [];
+
+    search.value?.split(",").forEach(filter => {
+        const filterNameToKey = {
+            assetcategory: 'assetCategory',
+            assetidentifier: 'assetIdentifier',
+            assettype: 'assetType',
+            category: 'assetCategory',
+            identifier: 'assetIdentifier',
+            location: 'location',
+            type: 'assetType',
+        };
+
+        // Step 1: determine type of filter
+        //  Types: =, != [equality], <, >, >=, <= [range]
+        const isRange = /^[^<>=]*[^<>=\s][^<>=]*(((<=|>=)[\s\S]*\S\s*)|([<>]([^=\s]|[^=]\S)[\s\S]*))+$/i.test(filter);
+        const isInequality = !isRange && /^[^<>=]*[^<>=\s][^<>=]*!=[\s\S]+$/i.test(filter);
+        const isEquality = !isRange && !isInequality && /^[^<>=]*[^<>=\s][^<>=]*(=[\s\S]+)?$/i.test(filter);
+        let isAlert = false;
+        
+        if (!isRange && !isEquality && !isInequality) return;
+        let filterKey = filter.match(/^[^<>=]*[^<>=\s!][^<>=!]*/i)?.[0]?.trim()?.toLowerCase();
+        let filterValue = filter.match(isRange
+        ? /(((<=|>=)[\s\S]*\S\s*)|([<>]([^=\s]|[^=]\S)[\s\S]*))+$/i
+        : /(=[\s\S]+)$/i)?.[0] ?? "";
+        
+        // Step 2: determine if filter key is valid
+        if (filterNameToKey?.[filterKey] === undefined)
+        {
+            if ((filterKey.match(/^alerts\.[\s\S]+/i)?.[0] ?? null) !== null)
+            {
+                filterKey = filterKey.substring(7);
+                isAlert = true;
+            }
+            else return;
+        }
+        else filterKey = filterNameToKey[filterKey];
+
+        // Step 3: create function which takes in the asset and outputs a boolean based on filter value and add it to filters
+        console.log(isInequality)
+        if (isEquality)
+        {
+            filterValue = filterValue.replace(/^=/i, "")?.trim();
+            filters.push((asset) => {
+                const value = asset[filterKey];
+                return new RegExp(value, "i").test(filterValue) || new RegExp(filterValue, "i").test(value);
+            });
+        }
+        else if (isInequality)
+        {
+            filterValue = filterValue.replace(/^!=/i, "")?.trim();
+            filters.push(asset => {
+                const value = asset[filterKey];
+                return !(new RegExp(value, "i").test(filterValue) || new RegExp(filterValue, "i").test(value));
+            })
+        }
+        else if (isRange)
+        {
+            const gte = [];
+            const lte = [];
+            const gt = [];
+            const lt = [];
+
+            while ((filterValue?.length ?? 0) > 0)
+            {
+                console.log("Testing...")
+                if (filterValue.startsWith(">="))
+                {
+                    filterValue = filterValue.substring(2);
+                    const nextValue = filterValue.match(/[^<>=]+/i)?.[0];
+                    if (nextValue !== undefined) gte.push(nextValue);
+                    filterValue = filterValue.substring(nextValue?.length ?? 0);
+                }
+                else if (filterValue.startsWith("<="))
+                {
+                    filterValue = filterValue.substring(2);
+                    const nextValue = filterValue.match(/[^<>=]+/i)?.[0];
+                    if (nextValue !== undefined) lte.push(nextValue);
+                    filterValue = filterValue.substring(nextValue?.length ?? 0);
+                }
+                else if (filterValue.startsWith(">"))
+                {
+                    filterValue = filterValue.substring(1);
+                    const nextValue = filterValue.match(/[^<>=]+/i)?.[0];
+                    if (nextValue !== undefined) gt.push(nextValue);
+                    filterValue = filterValue.substring(nextValue?.length ?? 0);
+                }
+                else if (filterValue.startsWith("<"))
+                {
+                    filterValue = filterValue.substring(1);
+                    const nextValue = filterValue.match(/[^<>=]+/i)?.[0];
+                    if (nextValue !== undefined) lt.push(nextValue);
+                    filterValue = filterValue.substring(nextValue?.length ?? 0);
+                }
+                else filterValue = filterValue.substring(1);
+            }
+
+            if (gte.length > 0) console.log(gte);
+            if (lte.length > 0) console.log(lte);
+            if (gt.length > 0) console.log(gt);
+            if (lt.length > 0) console.log(lt);
+        }
+    });
+
+    const result = (search.value ?? null) === null ? assets.value : assets.value.filter(asset => filters.every(filter => filter(asset)));
+
+    // Sort the list to be organized a bit better (but still allow for manual re-sorting)
+    result.sort((a, b) => {
+        if (a.assetCategory > b.assetCategory) return 1;
+        if (a.assetCategory < b.assetCategory) return -1;
+        if (a.assetType > b.assetType) return 1;
+        if (a.assetType < b.assetType) return -1;
+        if (a.assetIdentifier > b.assetIdentifier) return 1;
+        if (a.assetIdentifier < b.assetIdentifier) return -1;
+        if (a.location > b.location) return 1;
+        if (a.location < b.location) return -1;
+        return 0;
+    });
+
+    return result;
 });
 const loadingAssets = ref(false);
 
@@ -53,14 +175,31 @@ const retrieveAssets = () => {
     AssetServices.getAllAssets()
     .then(response => {
         assets.value = response.data.map(asset => {
-            return {
+            const result = {
                 id: asset.id,
                 assetCategory: asset.type.category.name,
                 assetType: asset.type.name,
                 assetIdentifier: asset.type.identifier?.assetData?.value,
-                location: !!asset.location ? `${asset.location.building.abbreviation} ${asset.location.name}` : `No location`,
+                // For location, we're adding the tilde to be removed when displaying since only sorting against borrower names causes a weird-looking sort, which the tilde fixes
+                location: !!asset.location ? `${asset.location.building.abbreviation} ${asset.location.name}` : !!asset.borrower ? `${asset.borrower.fName} ${asset.borrower.lName}` : `~No location`,
+                borrowerId: asset.borrower?.id,
                 alerts: asset.alerts.map(alert => { return { id: alert.id, type: alert.type.name, date: format(alert.date) } }),
             };
+
+            if ((result.borrowerId ?? null) !== null)
+            {
+                let parsedDate;
+                try {
+                    parsedDate = format(asset.dueDate);
+                }
+                catch {
+                    parsedDate = "No Due Date";
+                }
+
+                result.alerts = [{ id: 'Due Date', type: 'Due By', date: parsedDate }, ...result.alerts];
+            }
+
+            return result;
         });
         loadingAssets.value = false;
     })
@@ -88,6 +227,10 @@ const deleteAsset = (assetId) => {
     });
 };
 
+const checkInAsset = (assetId) => {
+    console.log("Checking in asset with id of " + assetId);
+};
+
 retrieveAssets();
 </script>
 
@@ -109,6 +252,7 @@ retrieveAssets();
                             hide-details
                             single-line
                             full-width
+                            clearable
                         />
                     </v-col>
                 </v-row>
@@ -127,6 +271,17 @@ retrieveAssets();
                 :headers="headers"
                 :items="filteredAssets"
             >
+                <template v-slot:item.location="{ item }">
+                    <td v-if="(item.borrowerId ?? null) !== null">
+                        Checked Out to 
+                        <router-link :to="router.resolve({ name: 'person', params: { id: item.borrowerId }})">
+                            {{ item.location }}
+                        </router-link>
+                    </td>
+                    <td v-else>
+                        {{ (item.location?.match("~No location")?.length ?? 0) > 0 ? item.location.replace("~", "") : item.location }}
+                    </td>
+                </template>
                 <template v-slot:expanded-row="{ columns, item }">
                     <tr
                         v-for="alert in item.alerts"
@@ -142,6 +297,14 @@ retrieveAssets();
                     </tr>
                 </template>
                 <template v-slot:item.actions="{ item }">
+                    <v-btn
+                        v-if="(item.borrowerId ?? null) !== null"
+                        class="ma-2"
+                        color="accent"
+                        @click="checkInAsset(item.id)"
+                    >
+                        Check In
+                    </v-btn>
                     <v-btn
                         class="ma-2"
                         color="primary"
